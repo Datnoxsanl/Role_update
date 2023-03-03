a25efcdd61f247717e195b028861e481994b1308
ki3nAMK <123nguyenbahoangkien123@gmail.com>
2023-03-03 23:21:03 +0700

    done nha ban

--- js.js
+++ js.js
@@ -1,7 +1,38 @@
-const table = document.querySelector("table");
+const tbody = document.querySelector("tbody");
 const inputArr = document.querySelectorAll("input");
-const th = document.querySelectorAll("th") ;
 
-document.querySelector(".update_btn").addEventListener( "mouseover", (event) => {
-    console.log(1) ;
+document.querySelector(".update_btn").addEventListener("click", (event) =>{
+    let name = inputArr[0].value ; 
+    let STCode = inputArr[1].value ; 
+    let position = inputArr[2].value ; 
+    if(name==""||STCode==""||position=="") return ; 
+    let tr = document.createElement("tr") ;
+    tr.innerHTML = `
+                                  <th>${document.querySelectorAll("th").length-3}</th>
+                                  <td>${name}</td>
+                                  <td>${STCode}</td>
+                                  <td>${position}</td>
+    `
+    tbody.append(tr) ;
+    inputArr.forEach( (element) => {
+        element.value = "" ;
+    })
+})
+
+document.querySelector(".delete_btn").addEventListener("click" , (event) => {  
+    let name = inputArr[0].value ; 
+    let STCode = inputArr[1].value ; 
+    let position = inputArr[2].value ; 
+    tbody.querySelectorAll("td").forEach( (element,index,array) => {
+        // console.log(name.trim(),"==",element.innerText,"\t",name.trim()==element.innerText) ;
+        if(element.innerText==name.trim() && array[index+1].innerText==position.trim() && array[index+2].innerText==STCode.trim()) {
+            tbody.removeChild(element.parentNode) ;
+        } 
+    })
+    let pos = 1 ; 
+    document.querySelectorAll("th").forEach( (element,index,array) => {
+        if(index>3) {
+            element.innerText = pos; pos++ ;  
+        } 
+    })
 })
