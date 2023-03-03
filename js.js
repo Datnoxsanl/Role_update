708150c397eca517e41406a0d36bc0da6163b13d
ki3nAMK <123nguyenbahoangkien123@gmail.com>
2023-03-03 21:55:39 +0700

    dau me sao cai file no k chay nho

--- js.js
+++ js.js
@@ -1,21 +1,7 @@
-const table= document.querySelector("table");
+const table = document.querySelector("table");
 const inputArr = document.querySelectorAll("input");
 const th = document.querySelectorAll("th") ;
 
-document.querySelector(".update_btn").addEventListener("click", (event) =>{
-    console.log("datml") ;
-    let name = inputArr[0].value ; console.log(name) ;
-    let STCode = inputArr[1].value ; console.log(STCode) ;
-    let position = inputArr[2].value ; console.log(position) ;
-    let tbody = document.createElement("tbody") ;
-    tbody.innerHTML = `
-         <tr>
-                                  <th>${th.length}</th>
-                                  <td>${name}</td>
-                                  <td>${STCode}</td>
-                                  <td>${position}</td>
-        </tr>
-    `
-    name = "" ; STCode = "" ; position = "" ;
+document.querySelector(".update_btn").addEventListener( "mouseover", (event) => {
+    console.log(1) ;
 })
-
